729f05dedd2e58f29e9ae7f09d3c7ea35a139e68
Даниил Косьянов <bazyan99@outlook.com>
2021-05-18 15:00:54 +0300

    Add project files.

---
+++ RSA/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        byte[] Encrypt(byte[] data, RSAParameters RSAKey, bool fOAEP)
+        {
+            byte[] encryptedData;
+            using (RSACryptoServiceProvider rSACryptoServiceProvider = new RSACryptoServiceProvider())
+            {
+                rSACryptoServiceProvider.ImportParameters(RSAKey);
+                encryptedData = rSACryptoServiceProvider.Encrypt(data, fOAEP);
+            }
+            return encryptedData;
+        }
+        byte[] Decrypt(byte[] data, RSAParameters RSAKey, bool fOAEP)
+        {
+            byte[] decryptedData;
+            using (RSACryptoServiceProvider rSACryptoServiceProvider = new RSACryptoServiceProvider())
+            {
+                rSACryptoServiceProvider.ImportParameters(RSAKey);
+                decryptedData = rSACryptoServiceProvider.Decrypt(data, fOAEP);
+            }
+            return decryptedData;
+        }
+
+        UnicodeEncoding unicodeEncoding = new UnicodeEncoding();
+        RSACryptoServiceProvider rSACryptoServiceProvider = new RSACryptoServiceProvider();
+        byte[] data;
+        byte[] encryptData;
+
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            data = unicodeEncoding.GetBytes(textBox1.Text);
+            encryptData = Encrypt(data, rSACryptoServiceProvider.ExportParameters(false), false);
+            textBox2.Text = unicodeEncoding.GetString(encryptData);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] data = Decrypt(encryptData, rSACryptoServiceProvider.ExportParameters(true), false);
+            textBox3.Text = unicodeEncoding.GetString(data);
+        }
+    }
+}
